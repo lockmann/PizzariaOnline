a57fef5346cd43d19724ebc1a07d488c214fac08
Henrique Lockmann <henriquelockmann@gmail.com>
2021-11-14 10:13:43 -0300

    adicionando informações das pizzas

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const s = (el)=> document.querySelector(el);
 const sa = (el)=> document.querySelectorAll(el);
 
 
-pizzaJson.map((item, index)=>{
-	let pizzaItem = c('.models .pizza-item').cloneNode(true);
-
-	c('.pizza-area').append(pizzaItem);
+pizzaJson.map((pizza, index)=>{
+	let pizzaItem = s('.models .pizza-item').cloneNode(true);
+	pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
+	pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
+	pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
+	pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
+	s('.pizza-area').append(pizzaItem);
 });
