4c73fd77981693b7aa9390fe6b4b71caa79178e0
Henrique Lockmann <henriquelockmann@gmail.com>
2021-11-14 10:53:05 -0300

    Abrindo modal

--- script.js
+++ script.js
@@ -8,5 +8,20 @@ pizzaJson.map((pizza, index)=>{
 	pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
 	pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
 	pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
+	
+	// seleciona o link atarvez da tag A, adiciona um evento click, cria-se a função do evento 
+	pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+		//desabilita a função padrão do envento, no caso ao clicar ele não atualiza a tela. 
+		e.preventDefault();
+
+
+		s('.pizzaWindowArea').style.opacity = 0;
+		s('.pizzaWindowArea').style.display = 'flex';
+		setTimeout(()=>{
+			s('.pizzaWindowArea').style.opacity = 1;
+		}, 200)
+		
+	})
+
 	s('.pizza-area').append(pizzaItem);
 });
